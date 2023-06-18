16b04caae7c499ec31f8dc55955315d6d4580550
sirdzzz <chebotarev.da@phystech.edu>
2023-06-18 13:53:32 +0300

    Redacted .gitignore

--- src/main/java/starter/PyStart.java
+++ src/main/java/starter/PyStart.java
@@ -8,15 +8,14 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
-// java -jar PyStart imagePath resultPath
 public class PyStart {
     public static void main(String[] args) throws IOException, InterruptedException {
-        if (args.length < 2){
-            throw new IllegalArgumentException("Exactly 2 parameters required !");
+        String imagePath = "src/main/resources/man-and-table.jpeg";
+        String resultPath = "src/main/results/resultJava.json";
+        if (args.length == 2){
+            imagePath = args[0];
+            resultPath = args[1];
         }
-        String imagePath = args[0];
-        String resultPath = args[1];
 
         PyController pyController = new PyController("python3",
                 "src/main/python/nn-v2.py");
